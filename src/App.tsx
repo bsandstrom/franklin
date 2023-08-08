f2a7fb7a23ccf7e9a8f33b4db3d21af02f268f26
Brent Sandstrom <30700614+bsandstrom@users.noreply.github.com>
2023-08-07 22:59:38 -0400

    Initial commit! There's not much here yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChartExample } from './ChartExample';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        header!
       </header>
+      <body>
+        <h1>
+          This is a website for franklin nh
+        </h1>
+        <ChartExample />
+      </body>
     </div>
   );
 }
